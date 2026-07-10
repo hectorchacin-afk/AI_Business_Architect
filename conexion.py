4c23148135ee7da59b95971c3d71b327f83c1930
Hector Chacin <hectorchacin20@gmail.com>
2026-07-09 22:37:15 -0400

    Logro: Se corrige la ruta HTTP POST y se valida transmision de datos en vivo

--- conexion.py
+++ conexion.py
@@ -1,15 +1,21 @@
 import requests
 
 try:
-    print("🌐 Iniciando petición HTTP al servidor remoto...")
-    respuesta = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+    print("🚀 Iniciando envío de datos (Petición POST) al servidor remoto...")
     
-    if respuesta.status_code == 200:
-        print("✅ CONEXIÓN EXITOSA: El servidor internacional respondió correctamente.")
-        datos_servidor = respuesta.json()
-        print(f"Datos recibidos en vivo: {datos_servidor}")
+    alerta_inventario = {
+        "repuesto": "Bujia Champion",
+        "estatus": "CRITICO",
+        "cantidad": 20
+    }
+    
+    respuesta = requests.post("https://jsonplaceholder.typicode.com/posts", json=alerta_inventario)
+    
+    if respuesta.status_code == 201:
+        print("✅ ENVÍO EXITOSO: El servidor remoto recibió y procesó la alerta.")
+        print(f"Respuesta oficial de la nube: {respuesta.json()}")
     else:
-        print(f"⚠️ ALERTA DE RED: Código de estado inesperado: {respuesta.status_code}")
+        print(f"⚠️ ALERTA DE RED: El servidor respondió con código {respuesta.status_code}")
 
 except requests.exceptions.ConnectionError:
     print("🚨 ERROR CRÍTICO: No se pudo establecer la conexión física a internet.")
